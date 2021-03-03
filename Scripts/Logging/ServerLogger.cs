a5521aa5de9c630031d4aebf64672035b235e1b4
Michael March <theneqstech@gmail.com>
2021-03-03 10:03:09 +0100

    Rename Logger to ServerLogger

--- Scripts/Logging/ServerLogger.cs
+++ Scripts/Logging/ServerLogger.cs
@@ -2,13 +2,13 @@ using SharedUtils.Scripts.Logging;
 
 namespace ServersUtils.Scripts.Logging
 {
-    public sealed class Logger : SharedUtils.Scripts.Logging.Logger
+    public sealed class ServerLogger : Logger
     {
         // one instance of LoggerFile for every Logger instance just in case
         private static BasicLogger _server;
-        public static BasicLogger Server => _server;
+        public static BasicLogger GetLogger => _server;
 
-        public Logger()
+        public ServerLogger() : base()
         {
             // _loggerFile passed by reference to always write to the same file
             _server = new BasicLogger(ref _loggerFile, "SERVER");
